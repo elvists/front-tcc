9ee940e0781b123a2155e447972554478a017fcb
Elvis Teles <elvis.silva@solutis.com.br>
2019-04-17 18:20:55 -0300

    Ajustes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,132 @@
 import React, { Component } from 'react';
 import './App.css';
 import Configuration from './components/configuration/Configuration';
+import 'bootstrap/dist/css/bootstrap.css';
+import Axios from 'axios';
+import Popup from './components/popup/Popup';
+
+import DropDownOption from './components/dropDownOption/DropDownOption'
+
+// import { Link } from 'react-router-dom';
+import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+
+var algorithms = { itemLabel: "Algoritmo", itemOptions: [{ label: "Média Global", value: "GlobalAvg" }, { label: "AR", value: "AR" }, { label: "Média dos Itens", value: "ItemAvg" }, { label: "Mais Popular", value: "MostPop" }] }
+var datasets = { itemLabel: "Conjunto de Dados", itemOptions: [{ label: "Filmes", value: "datasets/FilmTrust/ratings.txt" }] }
+var itemRanking = { itemLabel: "ItemRanking?", itemOptions: [{ label: "Sim", value: "on" }, { label: "Não", value: "off" }] }
+
+var apiBaseUrl = "http://localhost:8080/";
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      dataset: "",
+      recommender: "",
+      itemRanking: "",
+      showPopup: false
+    }
+
+  }
+  handleClick() {
+    var self = this;
+    var payload = {
+      "dataset": this.state.dataset,
+      "recommender": this.state.recommender,
+      "evaluationSetup": "cv -k 5 -p on --rand-seed 1 --test-view all",
+      "itemRanking": this.state.itemRanking + " -topN -1 -ignore -1",
+      "outputSetup": "on -dir results/",
+      "ratingSetup": "-columns 0 1 2 -threshold -1"
+
+    }
+
+    Axios.post(apiBaseUrl + 'recomendar', payload)
+      .then(function (response) {
+        if (response.status === 200) {
+          self.setState({
+            showPopup: true,
+            result: response.data
+          })
+          console.log(response.data);
+        } else {
+          console.log(response);
+          alert("Erro");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  changeDataSet(event) {
+    this.setState({ dataset: event }, () => {
+      this.afterSetStateFinished();
+    });
+  }
+
+  
+  afterSetStateFinished() {
+  }
+
+
+  changeItemRanking(event) {
+    this.setState({ itemRanking: event }, () => {
+      this.afterSetStateFinished();
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="Card">
+        <Card>
+          <CardBody>
+            <DropDownOption changeOption={this.changeDataSet.bind(this)} item={datasets} />
+            <DropDownOption changeOption={this.changeItemRanking.bind(this)} item={itemRanking} />
+          </CardBody>
+        </Card>
+        <Row>
+        <Col sm="4">
+            <Card>
+              <CardBody>
+              <Configuration algorithms={algorithms} />
+              </CardBody>
+            </Card>
+          </Col>      
+          <Col sm="4">
+            <Card>
+              <CardBody>
+              <Configuration algorithms={algorithms} />
+              </CardBody>
+            </Card>
+          </Col>      
+          <Col sm="4">
+            <Card>
+              <CardBody>
+              <Configuration algorithms={algorithms} />
+              </CardBody>
+            </Card>
+          </Col>             
+        </Row>
 
-          <Configuration />
-        </div>
-      </div>
+        <Button color="primary" >button</Button>
+        {/* <div className="Card">
+          <Configuration algorithms={algorithms} />
+          <div>
+            <RaisedButton className="RaisedButton" label={"Executar"} primary={true} onClick={(event) => this.handleClick(event)} />
+          </div>
+          {this.state.showPopup ?
+            <Popup
+              result={this.state.result}
+              isItemRanking={this.state.itemRanking}
+              closePopup={this.togglePopup.bind(this)}
+            />
+            : null
+          }
+        </div> */}
+      </div >
     );
   }
 }
 
 export default App;
+
--- src/components/configuration/Configuration.js
+++ src/components/configuration/Configuration.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
 import DropDownOption from '../dropDownOption/DropDownOption'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
-import Axios from 'axios';
-import Popup from '../popup/Popup';
-
-var algorithms = {itemLabel: "Algoritmo", itemOptions : [{ label: "Média Global", value: "GlobalAvg" }, { label: "AR", value: "AR" }, { label: "Média dos Itens", value: "ItemAvg" }, { label: "Mais Popular", value: "MostPop" }]}
-var datasets = {itemLabel: "Conjunto de Dados", itemOptions :[{ label: "Filmes", value: "datasets/FilmTrust/ratings.txt" }]}
-var itemRanking = {itemLabel: "ItemRanking?", itemOptions :[{ label: "Sim", value: "on" }, { label: "Não", value: "off" }]}
-
-
-var apiBaseUrl = "http://localhost:8080/";
 
 class Configuration extends Component {
 
     constructor() {
         super();
         this.state = {
-            dataset: "",
             recommender: "",
-            itemRanking: "",
             showPopup: false
         }
 
@@ -31,51 +18,14 @@ class Configuration extends Component {
         });
     }
 
-    handleClick() {
-        var self = this;
-        var payload = {
-            "dataset": this.state.dataset,
-            "recommender": this.state.recommender,
-            "evaluationSetup": "cv -k 5 -p on --rand-seed 1 --test-view all",
-            "itemRanking": this.state.itemRanking + " -topN -1 -ignore -1",
-            "outputSetup": "on -dir results/",
-            "ratingSetup": "-columns 0 1 2 -threshold -1"
 
-        }
-        Axios.post(apiBaseUrl + 'recomendar', payload)
-            .then(function (response) {
-                if (response.status === 200) {
-                    self.setState({
-                        showPopup: true,
-                        result: response.data
-                    })
-                    console.log(response.data);
-                } else {
-                    console.log(response);
-                    alert("Erro");
-                }
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
     changeAlgorithm(event) {
         this.setState({ recommender: event }, () => {
             this.afterSetStateFinished();
         });
     }
 
-    changeDataSet(event) {
-        this.setState({ dataset: event }, () => {
-            this.afterSetStateFinished();
-        });
-    } 
-    
-    changeItemRanking(event) {
-        this.setState({ itemRanking: event }, () => {
-            this.afterSetStateFinished();
-        });
-    }
+
 
     afterSetStateFinished() {
     }
@@ -84,24 +34,11 @@ class Configuration extends Component {
 
     render() {
         return (
-            <MuiThemeProvider>
                 <div>
-                    <DropDownOption changeOption={this.changeAlgorithm.bind(this)} item={algorithms} />
-                    <DropDownOption changeOption={this.changeDataSet.bind(this)} item={datasets} />
-                    <DropDownOption changeOption={this.changeItemRanking.bind(this)} item={itemRanking} />
-                    <div>
-                        <RaisedButton className="RaisedButton" label={"Executar"} primary={true} onClick={(event) => this.handleClick(event)} />
-                    </div>
-                    {this.state.showPopup ? 
-                        <Popup
-                        result={this.state.result}
-                        isItemRanking={this.state.itemRanking}
-                        closePopup={this.togglePopup.bind(this)}
-                        />
-                        : null
-                    }
+                    <DropDownOption changeOption={this.changeAlgorithm.bind(this)} item={this.props.algorithms} />
+                    {/* <DropDownOption changeOption={this.changeDataSet.bind(this)} item={datasets} />
+                    <DropDownOption changeOption={this.changeItemRanking.bind(this)} item={itemRanking} /> */}
                 </div>
-            </MuiThemeProvider>
         );
     }
 }
